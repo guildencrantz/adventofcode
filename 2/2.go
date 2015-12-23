222277cfce4a9ce553d9c8f6064a7454c44ab2bc
Matt Henkel <guildencrantz@menagerie.cc>
2015-12-22 21:08:22 -0700

    And 2.2 succeeds as well.
    Change-Id: Ic60c213165e2ee71c9c475957181caba77647cfc

---
+++ 2/2.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+type Package []int
+
+func NewPackage(s string) Package {
+	p := Package(make([]int, 3))
+	for i, d := range strings.Split(s, "x") {
+		p[i], _ = strconv.Atoi(d)
+	}
+	sort.IntSlice(p).Sort()
+	return p
+}
+
+func PaperNeeded(p Package) int {
+	return p[0]*p[1] + // Smallest side bonus paper
+		p[0]*p[1]*2 +
+		p[0]*p[2]*2 +
+		p[1]*p[2]*2
+}
+
+func RibbonNeeded(p Package) int {
+	return p[0]*2 +
+		p[1]*2 +
+		p[0]*p[1]*p[2]
+}
+
+func main() {
+	input, err := ioutil.ReadFile("./input")
+	if err != nil {
+		panic("Unable to read input")
+	}
+
+	paperNeeded := 0
+	ribbonNeeded := 0
+	for _, s := range strings.Split(string(input), "\n") {
+		p := NewPackage(s)
+		paperNeeded += PaperNeeded(p)
+		ribbonNeeded += RibbonNeeded(p)
+	}
+
+	fmt.Printf("Paper needed: %d sqft\n", paperNeeded)
+	fmt.Printf("Ribbon needed: %d ft\n", ribbonNeeded)
+}
